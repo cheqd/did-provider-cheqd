da3b454c8fcd448dfb96dd82597f7ee5966f80bf
Andrew Nikitin <andrew.nikitin@cheqd.io>
2022-05-18 17:51:43 +0300

    Fix build
    Signed-off-by: Andrew Nikitin <andrew.nikitin@cheqd.io>

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,3 @@ export { schema }
 export { CheqdDIDProvider } from './did-manager/cheqd-did-provider'
 export { CheqdDidResolver, getResolver } from './did-manager/cheqd-did-resolver'
 export { CheqdResolver } from './did-manager/resolver'
-export * from './types/IMyAgentPlugin'

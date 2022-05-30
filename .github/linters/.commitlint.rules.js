ee215814c9645094b8428c4b002a8d95f8bf35fa
Ankur Banerjee <ankurdotb@users.noreply.github.com>
2022-05-30 17:57:41 +0100

    Update commitlint rules

--- .github/linters/.commitlint.rules.js
+++ .github/linters/.commitlint.rules.js
@@ -30,7 +30,7 @@ module.exports = {
 		'subject-full-stop': [2, 'never', '.'],
 		'header-max-length': [2, 'always', 100],
 		'body-leading-blank': [1, 'always'],
-		'body-max-line-length': [2, 'always', 100],
+		'body-max-line-length': [2, 'always', 1000],
 		'footer-leading-blank': [1, 'always'],
 		'footer-max-line-length': [2, 'always', 100],
 	},

5e8623db191ee2ff257678ef564449390ef3c44d
jay-dee7 <jasdeepsingh.uppal@gmail.com>
2022-06-24 17:27:17 +0530

    feat: Added option to use Testnet or Mainnet
    Signed-off-by: jay-dee7 <jasdeepsingh.uppal@gmail.com>

--- src/did-manager/cheqd-did-provider.ts
+++ src/did-manager/cheqd-did-provider.ts
@@ -1,16 +1,23 @@
 import {
-  IIdentifier,
-  IKey,
-  IService,
-  IAgentContext,
-  IKeyManager,
-  ManagedKeyInfo,
+	IIdentifier,
+	IKey,
+	IService,
+	IAgentContext,
+	IKeyManager,
+	ManagedKeyInfo,
 } from '@veramo/core'
 import { AbstractIdentifierProvider } from '@veramo/did-manager'
 import Multibase from 'multibase'
 import Multicodec from 'multicodec'
 
 type IContext = IAgentContext<IKeyManager>
+
+export const DefaultTestnetRPCUrl = "https://rpc.cheqd.network";
+
+export enum NetworkType {
+	Mainnet = "mainnet",
+	Testnet = "testnet"
+}
 
 /**
  * You can use this template for an `AbstractIdentifierProvider` implementation.
@@ -26,117 +33,122 @@ type IContext = IAgentContext<IKeyManager>
  * @alpha
  */
 export class CheqdDIDProvider extends AbstractIdentifierProvider {
-  private defaultKms: string
+	private defaultKms: string
+	private readonly network: NetworkType;
+	private rpcUrl: string;
 
-  constructor(options: { defaultKms: string }) {
-    super()
-    this.defaultKms = options.defaultKms
-  }
+	constructor(options: { defaultKms: string, rpcUrl?: string, networkType?: NetworkType }) {
+		super()
+		this.defaultKms = options.defaultKms
+		this.network = options.networkType ? options.networkType : NetworkType.Testnet
+		this.rpcUrl = options.rpcUrl ? options.rpcUrl : DefaultTestnetRPCUrl;
+	}
 
-  async createIdentifier(
-      //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-    { kms, alias }: { kms?: string; alias?: string },
-    context: IContext,
-  ): Promise<Omit<IIdentifier, 'provider'>> {
-    const key: ManagedKeyInfo = await context.agent.keyManagerCreate({
-      kms: kms || this.defaultKms,
-      type: 'Ed25519',
-    })
+	async createIdentifier(
+		//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+		{ kms, alias }: { kms?: string; alias?: string },
+		context: IContext,
+	): Promise<Omit<IIdentifier, 'provider'>> {
+		const key: ManagedKeyInfo = await context.agent.keyManagerCreate({
+			kms: kms || this.defaultKms,
+			type: 'Ed25519',
+		})
 
-    const methodSpecificId = Buffer.from(
-      Multibase.encode(
-        'base58btc',
-        Multicodec.addPrefix(
-          'ed25519-pub',
-          Buffer.from(key.publicKeyHex, 'hex'),
-        ),
-      ),
-    )
-      .toString()
-      .substr(0, 32)
+		const methodSpecificId = Buffer.from(
+			Multibase.encode(
+				'base58btc',
+				Multicodec.addPrefix(
+					'ed25519-pub',
+					Buffer.from(key.publicKeyHex, 'hex'),
+				),
+			),
+		)
+			.toString()
+			.substr(0, 32)
 
-    const identifier: IIdentifier = {
-      did: 'did:cheqd:mainnet:' + methodSpecificId,
-      controllerKeyId: key.kid,
-      keys: [key],
-      services: [],
-      provider: 'cheqd',
-    }
+		const identifier: IIdentifier = {
+			did: `did:cheqd:${this.network}:` + methodSpecificId,
+			controllerKeyId: key.kid,
+			keys: [key],
+			services: [],
+			provider: 'cheqd',
+		}
 
-    // TODO: Implement custom debugger on creation.
+		// TODO: Implement custom debugger on creation.
+		return identifier
+	}
 
-    return identifier
-  }
+	async deleteIdentifier(
+		identity: IIdentifier,
+		context: IContext,
+	): Promise<boolean> {
+		for (const { kid } of identity.keys) {
+			await context.agent.keyManagerDelete({ kid })
+		}
+		return true
+	}
 
-  async deleteIdentifier(
-    identity: IIdentifier,
-    context: IContext,
-  ): Promise<boolean> {
-    for (const { kid } of identity.keys) {
-      await context.agent.keyManagerDelete({ kid })
-    }
-    return true
-  }
+	async addKey(
+		{
+			//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+			identifier,
+			//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+			key,
+			//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+			options,
+			//  eslint-disable-next-line @typescript-eslint/no-explicit-any
+		}: { identifier: IIdentifier; key: IKey; options?: any },
+		//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+		context: IContext,
+		//  eslint-disable-next-line @typescript-eslint/no-explicit-any
+	): Promise<any> {
+		throw Error('CheqdDIDProvider addKey not supported yet.')
+	}
 
-  async addKey(
-    {
-      //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-      identifier,
-      //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-      key,
-      //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-      options,
-      //  eslint-disable-next-line @typescript-eslint/no-explicit-any
-    }: { identifier: IIdentifier; key: IKey; options?: any },
-    //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-    context: IContext,
-    //  eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ): Promise<any> {
-    throw Error('CheqdDIDProvider addKey not supported yet.')
-  }
+	async addService(
+		{
+			//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+			identifier,
+			//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+			service,
+			//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+			options,
+			//  eslint-disable-next-line @typescript-eslint/no-explicit-any
+		}: { identifier: IIdentifier; service: IService; options?: any },
+		//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+		context: IContext,
+		//  eslint-disable-next-line @typescript-eslint/no-explicit-any
+	): Promise<any> {
+		throw Error('CheqdDIDProvider addService not supported yet.')
+	}
 
-  async addService(
-    {
-      //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-      identifier,
-      //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-      service,
-      //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-      options,
-      //  eslint-disable-next-line @typescript-eslint/no-explicit-any
-    }: { identifier: IIdentifier; service: IService; options?: any },
-    //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-    context: IContext,
-    //  eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ): Promise<any> {
-    throw Error('CheqdDIDProvider addService not supported yet.')
-  }
+	async removeKey(
+		//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+		args: {
+			identifier: IIdentifier;
+			kid: string;
+			//  eslint-disable-next-line @typescript-eslint/no-explicit-any
+			options?: any
+		},
+		//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+		context: IContext,
+		//  eslint-disable-next-line @typescript-eslint/no-explicit-any
+	): Promise<any> {
+		throw Error('CheqdDIDProvider removeKey not supported yet.')
+	}
 
-  async removeKey(
-      //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-    args: {
-      identifier: IIdentifier;
-      kid: string;
-      //  eslint-disable-next-line @typescript-eslint/no-explicit-any
-      options?: any },
-      //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-    context: IContext,
-      //  eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ): Promise<any> {
-    throw Error('CheqdDIDProvider removeKey not supported yet.')
-  }
-
-  async removeService(
-      //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-    args: {
-      identifier: IIdentifier;
-      id: string;
-      //  eslint-disable-next-line @typescript-eslint/no-explicit-any
-      options?: any },
-      //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-    context: IContext,
-      //  eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ): Promise<any> {
-    throw Error('CheqdDIDProvider removeService not supported yet.')
-  }
+	async removeService(
+		//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+		args: {
+			identifier: IIdentifier;
+			id: string;
+			//  eslint-disable-next-line @typescript-eslint/no-explicit-any
+			options?: any
+		},
+		//  eslint-disable-next-line @typescript-eslint/no-unused-vars
+		context: IContext,
+		//  eslint-disable-next-line @typescript-eslint/no-explicit-any
+	): Promise<any> {
+		throw Error('CheqdDIDProvider removeService not supported yet.')
+	}
 }

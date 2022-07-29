4573df529088a88d8dc8beb619caf198111139c7
Alexandr Kolesov <alexandr.kolesov@cheqd.io>
2022-07-29 17:55:00 +0300

    Add poc for custom methods registration

---
+++ src/agent/cheqd.ts
@@ -0,0 +1,42 @@
+import {
+    IAgentContext,
+    IKeyManager,
+    IAgentPlugin,
+    IPluginMethodMap,
+    IAgentPluginSchema
+} from '@veramo/core'
+import { CheqdDIDProvider } from '../did-manager/cheqd-did-provider'
+
+type IContext = IAgentContext<IKeyManager>
+
+export class Cheqd implements IAgentPlugin {
+    readonly methods?: IPluginMethodMap
+    readonly schema?: IAgentPluginSchema;
+
+    constructor(didProviderCheqd: CheqdDIDProvider) {
+        this.methods = {
+            'cheqdCreateIdentifierRaw': async (args: any, context: IContext): Promise<any> => {
+                if (!args.payload) {
+                    throw new Error('payload is required')
+                }
+
+                await didProviderCheqd.createIdentifierRaw(args.payload)
+            },
+        }
+
+        // z-schema rules
+        // https://github.com/zaggino/z-schema
+        this.schema = {
+            components: {
+                schemas: {},
+                methods: {
+                    'cheqdDidCreateRaw': {
+                        arguments: {},
+                        // This property is required
+                        returnType: {}
+                    }
+                }
+            }
+        }
+    }
+}

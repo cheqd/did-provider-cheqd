d15a6e49a3db8d17b2ec51865f2f981f38bc5403
Andrew Nikitin <andrew.nikitin@cheqd.io>
2024-01-23 20:00:39 +0100

    Add the same logic for deactivation and resource creation

--- src/did-manager/cheqd-did-provider.ts
+++ src/did-manager/cheqd-did-provider.ts
@@ -435,9 +435,50 @@ export class CheqdDIDProvider extends AbstractIdentifierProvider {
 		// Get Controllers
 		const controllers: string[] = getControllers(didDocument);
 
+		// For did deactivation ledger requires the signature from original DID Document controller
+        // So we need to add the controller to the list of controllers
+        if (!controllers.includes(didDocument.id)) {
+            controllers.push(didDocument.id);
+        }
+
+
 		// Generate payload
 		const versionId = options.versionId || v4();
 		const payload = await createMsgDeactivateDidDocPayloadToSign(didDocument, versionId);
+
+		// Compile signInfos
+		return await this.compileSignInfos(payload, controllers, options);
+	}
+
+	async resourceCreateCompileSignInfos(
+		did: string,
+		resourcePayload: ResourcePayload, 
+		options: {
+			context: IContext;
+			publicKeyHexs?: string[];
+		}): Promise<SignInfo[]> {
+
+		// Steps to solve the issue:
+		// 1. Collect list of controllers. The user can remove, append and reqrite the controller.
+		//   But we need to send all the signatures, old and news
+		// Generate payload to sign
+		// 3. Compile list of signInfos
+
+		const didDocument = await options.context.agent.resolveDid({ didUrl: did }).then((result) => result.didDocument);
+		if (!didDocument) {
+			throw new Error('[did-provider-cheqd]: resourceCreateCompileSignInfos: Erro while resolving the DID document for controller DID: ' + did);
+		}
+		// Get Controllers
+		const controllers: string[] = getControllers(didDocument);
+
+		// For resource creation ledger requires the signature from original DID Document controller
+        // So we need to add the controller to the list of controllers
+        if (!controllers.includes(did)) {
+            controllers.push(did);
+        }
+
+		// Generate payload
+		const payload = await MsgCreateResourcePayload.encode(MsgCreateResourcePayload.fromPartial(resourcePayload)).finish()
 
 		// Compile signInfos
 		return await this.compileSignInfos(payload, controllers, options);
@@ -497,77 +538,32 @@ export class CheqdDIDProvider extends AbstractIdentifierProvider {
 			provider: document.id.split(':').splice(0, 3).join(':'),
 		};;
 
-		if (options.publicKeyHexs) {
-			// It means that user
-			identifier.keys = await (async function (that: CheqdDIDProvider) {
-				const scopedKeys: ManagedKeyInfo[] = [];
-				for (const keyRef of options.publicKeyHexs!) {
-					try {
-						const managedKey = await context.agent.keyManagerGet({
-							kid: keyRef
-						} satisfies IKeyManagerGetArgs);
+		// Get keys for updated DIDDocument	
+		identifier.keys = options.keys
+			? await (async function (that: CheqdDIDProvider) {
+					const scopedKeys: ManagedKeyInfo[] = [];
+					for (const key of options.keys!) {
+						let managedKey: ManagedKeyInfo | undefined;
+						try {
+							managedKey = await context.agent.keyManagerImport({
+								...key,
+								kms: options.kms || that.defaultKms,
+							} satisfies MinimalImportableKey);
+						} catch (e) {
+							debug(`Failed to import key ${key.kid}. Reason: ${e}`);
+
+							// construct key, if it failed to import
+							managedKey = { ...key, kms: options.kms || that.defaultKms };
+						}
 						if (managedKey) {
 							scopedKeys.push(managedKey);
 						}
-					} catch (e) {
-						debug(`Failed to get key ${keyRef}. Reason: ${e}`);
 					}
-				}
-				return scopedKeys;
-			})(this);
-		} else {
-			// Otherwise - tries to get controllerKeyId from keys or from VerificationMethod
-			identifier.keys = options.keys
-				? await (async function (that: CheqdDIDProvider) {
-						const scopedKeys: ManagedKeyInfo[] = [];
-						for (const key of options.keys!) {
-							let managedKey: ManagedKeyInfo | undefined;
-							try {
-								managedKey = await context.agent.keyManagerImport({
-									...key,
-									kms: options.kms || that.defaultKms,
-								} satisfies MinimalImportableKey);
-							} catch (e) {
-								debug(`Failed to import key ${key.kid}. Reason: ${e}`);
 
-								// construct key, if it failed to import
-								managedKey = { ...key, kms: options.kms || that.defaultKms };
-							}
-							if (managedKey) {
-								scopedKeys.push(managedKey);
-							}
-						}
-
-						return scopedKeys;
-				})(this)
-				: await (async function (that: CheqdDIDProvider) { 
-					const vms: VerificationMethod[] = []
-					const controllers = getControllers(document);
-					// Otherwise, we need to get verification methods from didDocuments associated with list of controllers
-					for (const controller of controllers) {
-						// If controller === document.id, then we can to get verification methods from document
-						if (controller === document.id && document.verificationMethod) {
-                            document.verificationMethod.map((vm) => {
-                                vms.push(vm);
-                            })
-                        } else {
-							// Otherwise we need to get verification methods from didDocument associated with controller
-							const didDocument = await context.agent.resolveDid({ didUrl: controller }).then((result) => result.didDocument);
-							if (!didDocument) {
-								throw new Error('[did-provider-cheqd]: updateIdentifier: Error while resolving the DID document for controller DID: ' + controller);
-							}
-							if (!didDocument.verificationMethod) {
-								throw new Error('[did-provider-cheqd]: updateIdentifier: Controller DID document does not contain verification methods');
-							}
-							didDocument.verificationMethod.map((vm) => {
-								vms.push(vm);
-							})
-						}
-					}
-					return await that.getKeysFromVerificationMethod(context, vms);
-				})(this)
-			identifier.controllerKeyId = identifier.keys[0].kid;
-		}
+					return scopedKeys;
+			})(this)
+			: await this.getKeysFromVerificationMethod(context, document.verificationMethod);
+		identifier.controllerKeyId = identifier.keys[0].kid;
 
 		debug('Updated DID', did);
 
@@ -582,7 +578,12 @@ export class CheqdDIDProvider extends AbstractIdentifierProvider {
 		}: {
 			did: string;
 			document: DIDDocument;
-			options: { keys?: TImportableEd25519Key[]; fee?: DidStdFee; versionId?: string };
+			options: { 
+				keys?: TImportableEd25519Key[]; 
+				fee?: DidStdFee; 
+				versionId?: string,
+				publicKeyHexs?: string[]
+			};
 		},
 		context: IContext
 	): Promise<boolean> {
@@ -595,8 +596,11 @@ export class CheqdDIDProvider extends AbstractIdentifierProvider {
 					);
 				})()
 			: await (async function (that: CheqdDIDProvider) {
-					const data = await createMsgDeactivateDidDocPayloadToSign(document, versionId);
-					return await that.signPayload(context, data, document.verificationMethod);
+					return await that.deactivateIdentifierCompileSignInfos(document, {
+						context: context,
+						publicKeyHexs: options.publicKeyHexs,
+						versionId
+					});
 				})(this);
 
 		const tx = await sdk.deactivateDidDocTx(
@@ -619,7 +623,13 @@ export class CheqdDIDProvider extends AbstractIdentifierProvider {
 	async createResource(
 		{
 			options,
-		}: { options: { payload: ResourcePayload; signInputs?: ISignInputs[]; kms?: string; fee?: DidStdFee } },
+		}: { options: { 
+			payload: ResourcePayload; 
+			signInputs?: ISignInputs[]; 
+			kms?: string; 
+			fee?: DidStdFee
+			publicKeyHexs?: string[]
+		} },
 		context: IContext
 	): Promise<boolean> {
 		const sdk = await this.getCheqdSDK(options?.fee);
@@ -628,13 +638,10 @@ export class CheqdDIDProvider extends AbstractIdentifierProvider {
 			? options.signInputs
 			: await (async function (that: CheqdDIDProvider) {
 					const did = `did:cheqd:${that.network}:${options.payload.collectionId}`;
-					const { didDocument } = await sdk.queryDidDoc(did, { sdk: sdk });
-
-					return await that.signPayload(
-						context,
-						MsgCreateResourcePayload.encode(MsgCreateResourcePayload.fromPartial(options.payload)).finish(),
-						didDocument?.verificationMethod
-					);
+					return await that.resourceCreateCompileSignInfos(did, options.payload, {
+						context: context,
+						publicKeyHexs: options.publicKeyHexs,
+					});
 				})(this);
 
 		const tx = await sdk.createLinkedResourceTx(signInputs, options.payload, '', this?.fee, undefined, {

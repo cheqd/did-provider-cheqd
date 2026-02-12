36aa60cec6ca493c1c3ebfa5228a07f5fd37688b
DaevMithran <daevmithran1999@gmail.com>
2026-02-12 13:26:25 +0530

    Remove static init

--- src/did-manager/cheqd-did-provider.ts
+++ src/did-manager/cheqd-did-provider.ts
@@ -15,8 +15,6 @@ import {
 	ISignInputs,
 	IContext as ISDKContext,
 	CheqdNetwork,
-	FeemarketModule,
-	OracleModule,
 } from '@cheqd/sdk';
 import { MsgCreateResourcePayload } from '@cheqd/ts-proto/cheqd/resource/v2/index.js';
 import { AccountData, Coin, DirectSecp256k1HdWallet, DirectSecp256k1Wallet } from '@cosmjs/proto-signing';
@@ -582,10 +580,8 @@ export class CheqdDIDProvider extends AbstractIdentifierProvider {
 			});
 			const sdkOptions: ICheqdSDKOptions = {
 				modules: [
-					FeemarketModule as unknown as AbstractCheqdSDKModule,
 					DIDModule as unknown as AbstractCheqdSDKModule,
 					ResourceModule as unknown as AbstractCheqdSDKModule,
-					OracleModule as unknown as AbstractCheqdSDKModule,
 				],
 				rpcUrl: this.rpcUrl,
 				wallet: wallet,
